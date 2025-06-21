d0e201cc0e9883a236d4e50c537b4e58600b7749
Dmitry <d.a.demkin@gmail.com>
2025-06-21 15:13:49 +0500

    финиш

--- main.go
+++ main.go
@@ -1,1 +1,33 @@
 package main
+
+import "fmt"
+
+type Speaker interface {
+	speak()
+}
+
+type Human struct {
+	name string
+}
+
+type Robot struct {
+	serialNumb int
+}
+
+func (h Human) speak() {
+	fmt.Println("Меня звать", h.name)
+
+}
+
+func (r Robot) speak() {
+	fmt.Println("Мой номер", r.serialNumb, "я смотрю reelsы и курю айкос")
+}
+
+func main() {
+
+	var object1 Speaker = Human{"Василий"}
+	var object2 Speaker = Robot{153321}
+	object1.speak()
+	object2.speak()
+
+}
